ae2fe921cf390be43ead99158ec748070f82eb1d
黄腾霄 <huangtengxiao@cvte.com>
2018-09-28 09:04:51 +0800

    添加项目文件。

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Management;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT ApplicationID, Description, ID, LicenseStatus, Name FROM SoftwareLicensingProduct WHERE LicenseStatus=1");
+            foreach (var queryObj in searcher.Get())
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("SoftwareLicensingProduct instance");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("ApplicationID: {0}", queryObj["ApplicationID"]);
+                Console.WriteLine("Description: {0}", queryObj["Description"]);
+                Console.WriteLine("ID: {0}", queryObj["ID"]);
+                Console.WriteLine("LicenseStatus: {0}", queryObj["LicenseStatus"]);
+                Console.WriteLine("Name: {0}", queryObj["Name"]);
+            }
+            Console.Read();
+        }
+    }
+}
